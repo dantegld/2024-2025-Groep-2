067af9b8c1a33128b65d38a46947e3e29fe5579e
Dante Gallardo <159446379+dantegld@users.noreply.github.com>
2024-11-29 00:01:44 +0100

    Update installed.php

--- website/vendor/composer/installed.php
+++ website/vendor/composer/installed.php
@@ -19,6 +19,15 @@
             'aliases' => array(),
             'dev_requirement' => false,
         ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.9.2',
+            'version' => '6.9.2.0',
+            'reference' => 'a7b17b42fa4887c92146243f3d2f4ccb962af17c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
         'firebase/php-jwt' => array(
             'pretty_version' => 'v6.10.2',
             'version' => '6.10.2.0',
