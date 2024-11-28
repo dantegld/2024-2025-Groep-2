91ed4abdf26c632e38a7e579264021e125cd4910
Dante Gallardo Ledon <dante.gallardoledon@bazandpoort.be>
2024-11-29 00:03:54 +0100

    phpmailer try

--- website/connect.php
+++ website/connect.php
@@ -1,6 +1,6 @@
 <?php
-//$mysqli = new mysqli("auth-db1650.hstgr.io", "u578783310_groep2", "Groep22024", "u578783310_myshoes");
-$mysqli = new mysqli("localhost", "root", "", "u578783310_myshoes");
+$mysqli = new mysqli("auth-db1650.hstgr.io", "u578783310_groep2", "Groep22024", "u578783310_myshoes");
+//$mysqli = new mysqli("localhost", "root", "", "u578783310_myshoes");
 
 
 
