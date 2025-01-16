f555e7f6b7614747945e4572b2d055e464df5110
victorcoremans <coremans.vico@gmail.com>
2025-01-16 19:28:30 +0100

    alle gegevens van u bestellingen laten tonen maar is gekoppeld met vorige dus bevat geen producten

--- website/functies/adminSideMenu.php
+++ website/functies/adminSideMenu.php
@@ -18,6 +18,8 @@ echo '
 <a href= "promo">Promo Codes</a>
 <a href="recensieGoedkeuren">Approve reviews</a>
 <a href="lopendPromotieOverzicht">Active Promotions Overview</a>
+<a href="orders">Orders</a>
+
 </div>';
 
 ?>
