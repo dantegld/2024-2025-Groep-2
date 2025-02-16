dd505be3dcedb6abd82f80832c247912114d3fce
BibiYassine <yassine.b2007@hotmail.com>
2025-02-16 22:52:38 +0100

    contactform

--- website/contact.php
+++ website/contact.php
@@ -9,15 +9,23 @@
 <div style="position: absolute; top: 10px; left: 10px;">
                   <a href="index"><img src="images/shoes/goback.png" style="width: 50px; height: auto;"></a>
                </div>
-  <div class="form">
-<form action="https://formsubmit.co/contactmyshoes2800@gmail.com" method="POST">
-     <input type="text" name="name" required placeholder="Name">
-    <input type="email" name="email" required placeholder="Email">
-     <input type="text" name="subject" required placeholder="Subject">
-    <textarea name="message" required placeholder="Message"></textarea>
-    <input type="hidden" name="_captcha" value="false">
-     <button type="submit">Submit</button>
-</form>
-    
+
+               
+               <div class="contact-form" id="cont-form"data-aos="fade-up">
+           
+           <form action="send_mail.php" method="POST" id="contact-form">
+       <label for="name">Naam:</label><br>
+       <input type="text" id="name" name="name" required><br><br>
+
+       <label for="email">E-mailadres:</label><br>
+       <input type="email" id="email" name="email" required><br><br>
+
+       <label for="message">Bericht:</label><br>
+       <textarea id="message" name="message" rows="5" required placeholder=""></textarea><br><br>
+
+       <button type="submit">Verstuur</button>
+   </form>
+           </div>
+       </section>
 </body>
 </html>
